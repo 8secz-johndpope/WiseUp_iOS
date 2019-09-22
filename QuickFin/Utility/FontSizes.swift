d9aadc837b9ccce66615c801e7c29d843941275f
Jack Boyuan Xu <boyuanxu@usc.edu>
2019-09-22 01:04:13 -0700

    Important files Xcode somehow left out

---
+++ QuickFin/Utility/FontSizes.swift
@@ -0,0 +1,19 @@
+//
+//  Fonts.swift
+//  QuickFin
+//
+//  Created by Boyuan Xu on 9/21/19.
+//  Copyright © 2019 Fidelity Investments. All rights reserved.
+//
+
+import UIKit
+
+struct FontSizes {
+    static let largeNavTitle: CGFloat = 34
+    static let navTitle: CGFloat = 17
+    static let pageTitle: CGFloat = 17
+    static let secondaryTitle: CGFloat = 15
+    static let text: CGFloat = 13
+    static let tableHeader: CGFloat = 13
+    static let tabBarTitle: CGFloat = 10
+}
