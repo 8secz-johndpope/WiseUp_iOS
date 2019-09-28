052f37b4de192466a0d731d384bb04a79c25812e
Jack Boyuan Xu <boyuanxu@usc.edu>
2019-09-28 13:56:03 -0700

    Simplified Firebase document fetching logic for chapters and questions

--- QuickFin/Models/Chapter.swift
+++ QuickFin/Models/Chapter.swift
@@ -7,35 +7,10 @@
 //
 
 import Foundation
-import CodableFirebase
 
-class Chapter {
-    
+class Chapter: Codable {
     var name: String
     var questions: [Question]
     var id: Int
     var active: Bool
-    
-    init(name: String, questions: [Question], active: Bool, id: Int) {
-        self.name = name
-        self.questions = questions
-        self.active = active
-        self.id = id
-    }
-    
-    // Used by Firebase Utility to load Chapters from Firebase
-    init(data: [String: Any?]) {
-        self.name = data["name"] as! String
-        self.questions = [Question]()
-        
-        // Mess with the following four lines at your own risk
-        let questionDict = data["questions"] as! NSArray // Convert to NSArray to allow iteration
-        for dat in questionDict {
-            self.questions.append(Question(data: dat as! [String : Any])) // Cast to dictionary for constructor of Question
-        }
-        
-        self.active = data["active"] as! Bool
-        self.id = data["id"] as! Int
-    }
-    
 }
