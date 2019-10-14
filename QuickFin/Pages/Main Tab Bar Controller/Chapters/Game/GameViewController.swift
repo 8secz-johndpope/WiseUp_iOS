24a56c1625aed7609fd331c0299ee65ffea7d48b
ctbuckley <cbuckley@usc.edu>
2019-10-14 00:58:50 -0700

    Starting up the Game Controller

---
+++ QuickFin/Pages/Main Tab Bar Controller/Chapters/Game/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  QuickFin
+//
+//  Created by Connor Buckley on 10/14/19.
+//  Copyright © 2019 Fidelity Investments. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+    }
+    
+}
+
+// MARK: - UI
+extension GameViewController {
+    
+    func initUI() {
+        
+    }
+    
+}
