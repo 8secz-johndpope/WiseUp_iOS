be4a9f554ae65b8b57a7de3b2a5c8d885fb5f611
Jack Boyuan Xu <boyuanxu@usc.edu>
2019-10-19 00:16:13 -0700

    Added all error popups

--- QuickFin/AppDelegate.swift
+++ QuickFin/AppDelegate.swift
@@ -46,20 +46,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if let error = error {
-            #warning("TODO: Error popup")
-            print(error.localizedDescription)
+            ErrorMessageHandler.shared.showMessageModal(theme: .error, title: "Google sign in error", body: error.localizedDescription)
             return
         }
         guard let auth = user.authentication else {
-            #warning("TODO: Error popup")
-            print(error.localizedDescription)
+            ErrorMessageHandler.shared.showMessageModal(theme: .error, title: "Google sign in error", body: error.localizedDescription)
             return
         }
         let credential = GoogleAuthProvider.credential(withIDToken: auth.idToken, accessToken: auth.accessToken)
         Auth.auth().signIn(with: credential) { (result, error) in
             if let error = error {
-                #warning("TODO: Error popup")
-                print(error.localizedDescription)
+                ErrorMessageHandler.shared.showMessageModal(theme: .error, title: "Firebase sign in error", body: error.localizedDescription)
                 return
             }
             // Shouldn't need to do anything here due to the Auth state listener set previously.
---
+++ QuickFin/Utility/ErrorMessageHandler.swift
@@ -0,0 +1,47 @@
+//
+//  ErrorMessageHandler.swift
+//  QuickFin
+//
+//  Created by Boyuan Xu on 10/18/19.
+//  Copyright © 2019 Fidelity Investments. All rights reserved.
+//
+
+import UIKit
+import SwiftMessages
+import Localize_Swift
+
+class ErrorMessageHandler {
+    
+    static var shared = ErrorMessageHandler()
+    private init() {}
+    
+    private func getView(theme: Theme, title: String, body: String, buttonTitle: String? = nil) -> MessageView {
+        let errorView = MessageView.viewFromNib(layout: .cardView)
+        errorView.configureTheme(theme)
+        errorView.configureDropShadow()
+        errorView.configureContent(title: title.localized(), body: body)  // Error messages are typically pre-localized
+        if let buttonTitle = buttonTitle {
+            errorView.button?.setTitle(buttonTitle.localized(), for: .normal)
+        } else {
+            errorView.button?.setTitle("Okay".localized(), for: .normal)
+        }
+        _ = errorView.button?.reactive.tap.observeNext(with: { (_) in
+            SwiftMessages.hide()
+        })
+        return errorView
+    }
+    
+    func showMessage(theme: Theme, title: String, body: String, buttonTitle: String? = nil) {
+        let errorView = getView(theme: theme, title: title, body: body, buttonTitle: buttonTitle)
+        SwiftMessages.show(view: errorView)
+    }
+    
+    func showMessageModal(theme: Theme, title: String, body: String, buttonTitle: String? = nil) {
+        let errorView = getView(theme: theme, title: title, body: body, buttonTitle: buttonTitle)
+        var config = SwiftMessages.Config()
+        config.presentationContext = .window(windowLevel: .statusBar)
+        config.preferredStatusBarStyle = .lightContent
+        SwiftMessages.show(config: config, view: errorView)
+    }
+    
+}
