672110c2df37061a438ec7296130939729d278a4
Jack Boyuan Xu <boyuanxu@usc.edu>
2020-01-18 21:16:30 -0800

    Added missing file

---
+++ QuickFin/Utility/Text.swift
@@ -0,0 +1,21 @@
+//
+//  Text.swift
+//  QuickFin
+//
+//  Created by Boyuan Xu on 1/18/20.
+//  Copyright © 2020 Fidelity Investments. All rights reserved.
+//
+
+import Foundation
+
+struct Text {
+    static let TabBarChapter = "Chapter"
+    static let TabBarFriends = "Friends"
+    static let TabBarStore = "Store"
+    static let TabBarProfile = "Profile"
+    static let Placeholder = "..."
+    static let UserImageNamePlaceholder = "Blank User Icon"
+    static let Delete = "Delete"
+    static let Invite = "Invite"
+    static let FriendsNavTitle = "Friends"
+}
